941116f3d03796c33716c7985142393d97a686a8
shivani281989 <shivani.281989@gmail.com>
2018-05-30 20:22:15 +0400

    MicroserviceConfig first project

---
+++ MicroServiceConfigServer/src/main/java/com/example/MicroServiceConfigServer/MicroServiceConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.MicroServiceConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MicroServiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceConfigServerApplication.class, args);
+	}
+}
