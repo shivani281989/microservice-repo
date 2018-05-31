a02fac029f2f8ee3c58cf0e82b4591db31793236
shivani281989 <39770220+shivani281989@users.noreply.github.com>
2018-05-31 19:32:34 +0400

    third commit for MIC-1
    third commit for MIC-1

--- MicroServiceConfigServer/src/main/java/com/example/MicroServiceConfigServer/MicroServiceConfigServerApplication.java
+++ MicroServiceConfigServer/src/main/java/com/example/MicroServiceConfigServer/MicroServiceConfigServerApplication.java
@@ -10,6 +10,7 @@ public class MicroServiceConfigServerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("second commit for new branch MIC-1");
+		System.out.println("third commit for new branch MIC-1");
 		SpringApplication.run(MicroServiceConfigServerApplication.class, args);
 	}
 }
